2fe2868b84a85854f5838587f98a92bb8af0497a
jsr <jaskomal2@gmail.com>
2020-11-02 22:03:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |ele,idx| 
-      puts "#{idx}. #{ele}"
+      puts "#{idx + 1}. #{ele}"
   end
 end
